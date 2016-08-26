fb969072ccf289908a70fd7e054c6f2ba57b78c6
JiyanBlack <rglsm7655558@gmail.com>
2016-08-26 12:34:07 +0800

    VideoPipe 0.1

---
+++ public/client.js
@@ -0,0 +1,85 @@
+var urlMap = [];
+var counter = 0;
+
+$(document).ready(main);
+
+
+function main() {
+  $('#btn-add').on('click', addUrl);
+  $('#btn-deleteAll').on('click', deleteAll);
+  $('#btn-download').on('click', download);
+  $('#ul-url').on('click', 'button', deleteOne);
+  $('.alert').hide();
+}
+
+function showError(error) {
+  //show error to user
+  $('#error-area').text(error.toString());
+  $('.alert').fadeIn();
+}
+
+function hideError() {
+  //hide error
+  $('.alert').hide();
+}
+
+function addUrl() {
+  // add a single url
+  hideError();
+  var newurl = $('#input-url').val();
+  try {
+    verifyUrl(newurl);
+  } catch (e) {
+    showError(e);
+    return
+  }
+  urlMap.push(newurl);
+  showUrl(newurl);
+  counter += 1;
+  $('#input-url').val('');
+}
+
+function showUrl(newurl) {
+  // add single url to html page
+  var new_li = $('<li class="list-group-item" ' + 'data-id=' + counter.toString() +
+    '>' + newurl + '<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + '</li>');
+  $('#ul-url').append(new_li);
+}
+
+function deleteAll() {
+  //delete all urls
+  $('#ul-url').html('');
+  urlMap = [];
+}
+
+function download() {
+  //begin to download the downloading code;
+  hideError();
+  console.log('Start to download:' + urlMap.toString());
+  $.ajax({
+    type: 'POST',
+    url: '/download',
+    contentType: 'application/json',
+    data: JSON.stringify(urlMap)
+  }).done(function(fileName) {
+    window.open('/file/' + fileName);
+  }).fail(function(jqXHR, status, errorMessage) {
+    showError(errorMessage);
+  });
+}
+
+function deleteOne() {
+  // delete a single url
+  var li = $(this).closest('li');
+  var id = parseInt(li.data('id'));
+  console.log(id);
+  li.remove();
+  delete urlMap[id];
+  console.log(urlMap);
+}
+
+
+function verifyUrl(newurl) {
+  let validationReg = /^(http|https)\:\/\/www\.youtube\.com/;
+  if (!validationReg.exec(newurl)) throw new Error('Invalid Youtube url!');
+}
---
+++ server.js
@@ -0,0 +1,70 @@
+let express = require('express');
+let app = express();
+let bodyParse = require('body-parser');
+let archiver = require('archiver');
+let fs = require('fs');
+
+let urlencodedParser = bodyParse.urlencoded({ extended: false });
+let jsonParser = bodyParse.json();
+let venv_win = fs.readFileSync('./server/venv_win.zip')
+let fileArray = Object.create(null);
+app.use(express.static('public'));
+
+app.listen(8000, function() {
+  console.log('Listening on port 8000...');
+});
+
+app.route('/download')
+  .post(jsonParser, function(request, response) {
+    // receive a json file, download zip file
+    urlMap = request.body;
+    purifyUrl(urlMap);
+    try {
+      verifyUrl(urlMap);
+    } catch (e) {
+      response.sendStatus(500).send(e.toString());
+    }
+    //generate zipfiles for windows:
+    let zipFile = archiver('zip');
+    let fileName = (Math.floor(Date.now() * Math.random())).toString();
+
+    zipFile.append(JSON.stringify(urlMap), { name: 'urls.json' });
+    zipFile.append(venv_win, { name: 'venv_win.zip' });
+    zipFile.file('./server/unzip.exe', { name: 'unzip.exe' });
+    zipFile.file('./server/util.dat', { name: 'util.dat' });
+    zipFile.file('./server/RunMe.cmd', { name: 'RunMe.cmd' }).finalize();
+    fileArray[fileName] = zipFile;
+    //redirect to download the file
+    response.send(fileName);
+  });
+
+app.route('/file/:fileName').get(function(request, response) {
+  let fileName = request.params.fileName;
+  console.log("User downloading: " + fileName);
+  response.set({
+    'Content-Type': 'application/zip',
+    'Content-disposition': 'attachment; filename=' + fileName + '.zip'
+  });
+  fileArray[fileName].pipe(response);
+  fileArray[fileName].on('finish', function() {
+    delete fileArray[fileName];
+    console.log('Files Waiting for download: ' + fileArray.length);
+  });
+})
+
+
+function verifyUrl(urlMap) {
+  let validationReg = /^(http|https)\:\/\/www\.youtube\.com/;
+  urlMap.forEach(url => {
+    if (!validationReg.exec(url)) throw new Error('Invalid Youtube url!');
+  });
+  if (urlMap.length <= 0) throw new Error('No url is provided!')
+}
+
+function purifyUrl(urlMap) {
+  for (let i = 0; i < urlMap.length; i++) {
+    if (!urlMap[i]) {
+      urlMap.slice(0, i).concat(urlMap.slice(i + 1));
+    }
+  }
+}
